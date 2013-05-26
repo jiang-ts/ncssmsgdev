70b8004fbe31280125913b90628ccd5752c26c34
Michael Gao <mgao4793@gmail.com>
2013-05-25 23:29:26 -0400

    The first commit of the NCSSM SG test website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doit::Application.config.secret_token = '7cb9756039d3a38353988bebd0be4a11f3699792b015100d4af32c5907a3594154a4f28bf83a6738a9d0037f390308b512a9e1913ccf23de409b034ac3669316'
